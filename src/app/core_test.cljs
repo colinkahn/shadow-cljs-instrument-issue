5c63e82d6f685c4093ae2e609445f56778bb02d0
Colin Kahn <colin@parkside.app>
2019-12-12 12:26:00 -0800

    even more minimal

--- src/app/core_test.cljs
+++ src/app/core_test.cljs
@@ -5,25 +5,5 @@
             [cljs.test :as t :refer-macros [deftest is async testing]]))
 
 (deftest test-1
-  (async done
-    (st/unstrument)
-    (is (= (set (st/instrument)) #{`app.core/foo}))
-    (done)))
-
-(deftest test-2
-  (async done
-    (st/unstrument)
-    (is (= (set (st/instrument)) #{`app.core/foo}))
-    (done)))
-
-(deftest test-3
-  (async done
-    (st/unstrument)
-    (is (= (set (st/instrument)) #{`app.core/foo}))
-    (done)))
-
-(deftest test-4
-  (async done
-    (st/unstrument)
-    (is (= (set (st/instrument)) #{`app.core/foo}))
-    (done)))
+  (st/unstrument)
+  (is (= (set (st/instrument)) #{`app.core/foo})))
